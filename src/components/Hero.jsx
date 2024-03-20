ebca0d5c24ded4e101d3b9e7a3336ade6e6bf8a1
saisapura <sapurachewae@gmail.com>
2024-03-20 21:27:37 +0700

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
     >   
       <div className="container relative" ref={parallaxRef}><Gradient/>
      
-        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[.25rem] overflow-hidden">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[2rem] md:mb-20 lg:mb-[.25rem] overflow-hidden">
           <h1 className="h1 mb-12"> 
             Welcome to Muay Thai FightClub Gym
             <img
@@ -28,14 +28,15 @@ const Hero = () => {
               loading="lazy" 
             /> 
           </h1>
-          <p className="body-2 max-w-3xl mx-auto mb-12 text-n-2 lg:mb-12">
+          <p className="body-2 max-w-3xl mx-auto  text-n-2 lg:mb-12">
             In the heart of relentless training and expert guidance, we sculpt champions. Join us in the pursuit of greatness, where every session is a thrilling ascent towards your ultimate potential.
           </p>
-          <Button className="button mt-8" href="/pricing" white>
+          <Button className="button mt-7" href="/pricing" white>
             Get started
           </Button>
+         
         </div>
-      </div>
+      </div> 
     </Section>
   );
 };
